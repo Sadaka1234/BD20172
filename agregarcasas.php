d3cac74524edc9cae0fc80685d941446a297d654
Sadaka1234 <gonzalo.duran.saez@gmail.com>
2017-12-01 03:18:43 -0300

    Agregar Casas creo que funciona

--- agregarcasas.php
+++ agregarcasas.php
@@ -50,19 +50,30 @@ $userRow = pg_fetch_array($res);
             }
            echo "Hola ".$userRow['username'].". En el sistema eres ".$tipousuario." Que quieres?";  ?></li>
 
-	<form action="agregarcasasDB.php" method="POST">
-	<h3>Nombre Casa</h3>
-        	<input type="text" name="nombre_casa"><br/>
-	<h3>Cantidad de plata</h3>
-		<input type="text" name="cantidad_plata"><br/>
-  <h3>Nombre del Lider</h3>
-    <input type ="text" name="lider"><br/>
-  <h3>Fecha Inicio:</h3>
-    <input type ="date" name="ini"><br/>
- <h3>Fecha Termino:</h3>
-    <input type="date" name="fin"><br/>
-		<input type="submit" value = "Agregar">
-  </form>
+           <?php
+
+              $out =   ' 	<form action="agregarcasasDB.php" method="POST">
+                        	<h3>Nombre Casa</h3>
+                          <input type="text" name="nombre_casa"><br/>
+                        	<h3>Cantidad de plata</h3>
+                        	<input type="number" name="cantidad_plata"><br/>
+                          <h3>Fecha Inicio:</h3>
+                          <input type ="date" name="ini"><br/>
+                          <h3>Fecha Termino:</h3>
+                          <input type="date" name="fin"><br/>';
+                          $out .= '<h3>Nombre del Lider</h3>
+                          <select name="id_lider">';
+
+                       $personaje = pg_query($conn,'select * from personaje where id_casa IS NULL');
+                       while ($row = pg_fetch_row($personaje) ) {
+                         $out .= '<option value="'.$row[0].'">'.$row[1].'</option>';}
+                        $out .= '</select><br>';
+                        $out .= '<input type="submit" value="Agregar"> </form>';
+
+                       echo $out;
+               ?>
+
+
 
 	<?php
 	echo $_POST["nombre_casa"];
--- agregarcasasDB.php
+++ agregarcasasDB.php
@@ -32,17 +32,13 @@ $userRow = pg_fetch_array($res);
         <div class ="menu">
           <ul id="menu">
             <li><a href="index.php">Logout</a></li>
-            <li><a href="loged.php">Volver atrás</a></li>
+            <li><a href="agregarcasas.php">Volver atrás</a></li>
           </ul>
         </div>
 
         <div class="contenido">
           <li>
-<<<<<<< HEAD
 	    <?php
-=======
-	           <?php
->>>>>>> ac63cc8fde3d6471a8b512ec8718feb083ff5dfc
             $tipousuario = "";
 
             if ($userRow['PERMISO']=='t'){
@@ -53,22 +49,41 @@ $userRow = pg_fetch_array($res);
             }
            echo "Hola ".$userRow['username'].". En el sistema eres ".$tipousuario." Que quieres?";  ?></li>
 
-
-
  <?php
+$ncasa = $_POST["nombre_casa"];
+$id_lider = $_POST["id_lider"];
+$ini = $_POST["ini"];
+$fin = $_POST["fin"];
+$dinero = $_POST["cantidad_plata"];
 
-$guardarcasa = "INSERT INTO casa (nombre_casa, dinero_casa) VALUES('".$_POST["nombre_casa"]."',".$_POST["cantidad_plata"].");";
-
-$mauro = pg_query($conn,$guardarcasa);
-if (!$mauro) {
-	echo "Debes detenegte!!";
+$sql = "select * from personaje where id_personaje = ".$id_lider." and fecha_ini IS NULL";
+$query = pg_query($conn, $sql);
+if (!$sql){
+  echo "Ese personaje ya es lider de alguna casita, elige otro personaje";
 
 }
-else {
-	echo "Has agregado una casa correctamente!.";
-	}
-<<<<<<< HEAD
-=======
+else{
+   if ($ini > $fin){
+     echo "Todo termino antes de empezar... Revisa las fechas";
+     }
+   else{
+     $guardar = "INSERT INTO casa (dinero_casa , nombre ) VALUES (".$dinero.", '".$ncasa."')";
+     $sql = pg_query($conn, $guardar);
+     if (!$sql){
+       echo "error en el insert de casas";
+     }
+     else{
+       echo "Casa Creada";
+     }
+     $guardar = "UPDATE personaje set fecha_ini = '".$ini."', fecha_fin = '".$fin."', id_casa = currval('casa_id_casa_seq') where id_personaje = ".$id_lider;
+     $sql = pg_query($conn, $guardar);
+     if (!$sql) {
+         echo "ERror en el update";
+     }
+     else{
+        echo "Personaje Hecho Lider";
+     }
+   }
+   }
 
->>>>>>> ac63cc8fde3d6471a8b512ec8718feb083ff5dfc
  ?>
